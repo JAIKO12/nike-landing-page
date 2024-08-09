0a81ca0fa8bf5af934a95c79596b5ad6a7e6792d
NoufalTrianto <noufaltrianto@gmail.com>
2024-08-09 10:29:32 +0700

    Membuat landing page nike dengan tailwind

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import PopularProducts from './PopularProduct'
+import SpecialOffer from './SpecialOffer'
+import Subscribe from './Subscribe'
+import SuperQuality from './SuperQuality'
+import CustomerReviews from './CustomerReviews'
+import Footer from './Footer'
+import Services from './Services'
+
+export  {
+    Hero,
+    PopularProducts,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Footer,
+    Services
+}
