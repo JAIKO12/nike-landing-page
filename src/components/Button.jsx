1f85cf9f71944477aeef3b845ba29a3dfe8bf69f
NoufalTrianto <noufaltrianto@gmail.com>
2024-08-14 19:49:10 +0700

    menambahkan props di button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Button = ({label,iconUrl,backgroundColor, textColor, borderColor}) => {
+const Button = ({ label, iconUrl, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
-    <button className={`flex justify-center items-center px-7 py-4 border font-montserrat text-lg leading-none  bg-coral-red 
-    ${backgroundColor ? `border ${backgroundColor} ${textColor} ${borderColor}`: "bg-coral-red rounded-full text-white"} rounded-full `}>
+    <button
+      className={`flex justify-center items-center px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+      ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor ? `border ${borderColor}` : ''}` : 'bg-coral-red text-white'}
+      ${fullWidth ? 'w-full' : ''}`}>
         {label}
         {iconUrl && <img src={iconUrl} alt="" className='ml-2 rounded-full w-5 h-5' />}
     </button>
